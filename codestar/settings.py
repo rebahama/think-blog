b0afb1a5c9941ad62304e9443d334e64fbbed5cd
rebahama <wiri@hotmail.se>
2022-06-28 01:22:02 +0000

    add accounts

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,13 +43,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
+    
 ]
+SITE_ID = 1
 
+LOGIN_REDIRECT_URL= '/'
+LOGOUT_REDIRECT_URL= '/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -138,3 +146,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blogs_urls'),
+    path('accounts/',include('allauth.urls')),
 ]
